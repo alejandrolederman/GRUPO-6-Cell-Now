5daab72d1c27afafc53ea5f87f61b1040bf60828
aguian <aguian0104@gmail.com>
2021-10-26 18:53:39 -0300

    se agrega html y estilo al login

--- app.js
+++ app.js
@@ -12,21 +12,21 @@ app.listen (port, ()=>{
 });
 
 app.get("/", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
- }) ;
-
- app.get("/views/comprar.html", (req, res)=>{
-     res.sendFile(path.resolve(__dirname, "./views/comprar.html"))
- }) ;
-
- app.get("/views/login.html", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/login.html"))
-}) ;
+ }) ;
 
-app.get("/views/formulario-registro.html", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./views/formulario-registro.html"))
-}) ;
+//  app.get("/views/comprar.html", (req, res)=>{
+//      res.sendFile(path.resolve(__dirname, "./views/comprar.html"))
+//  }) ;
 
-app.get("/views/carrito-de-compras.html", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./views/carrito-de-compras.html"))
-}) ;
+//  app.get("/views/login.html", (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, "./views/login.html"))
+// }) ;
+
+// app.get("/views/formulario-registro.html", (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, "./views/formulario-registro.html"))
+// }) ;
+
+// app.get("/views/carrito-de-compras.html", (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, "./views/carrito-de-compras.html"))
+// }) ;
