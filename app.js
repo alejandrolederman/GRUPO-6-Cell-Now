c9051c461e49a8d1e6193ae9e58f52fcfb5912af
aguian <aguian0104@gmail.com>
2021-10-27 00:42:15 -0300

    arreglando rutas

--- app.js
+++ app.js
@@ -12,41 +12,22 @@ app.listen (port, ()=>{
 });
 
 app.get("/", (req, res)=>{
-<<<<<<< HEAD
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+    res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 
-app.get("/views/login", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./views/login.html"))
- });
-
-
-app.get("/views/comprar", (req, res)=>{
-     res.sendFile(path.resolve(__dirname, "./views/comprar.html"))
- });
-=======
-    res.sendFile(path.join(__dirname, "./views/home.html"))
- }) ;
-
-app.get("/views/login", (req, res)=>{
+app.get("/views/login.html", (req, res)=>{
     res.sendFile(path.join(__dirname, "./views/login.html"))
- }) ;
+ });
 
 
-app.get("/views/comprar", (req, res)=>{
-     res.sendFile(path.join(__dirname, "./views/comprar.html"))
- }) ;
->>>>>>> a6c95b45406dd3480326298ec7537a391b85e17a
-
-app.get('/views/register', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/formulario-registro.html'))
+app.get("/views/comprar.html", (req, res)=>{
+ res.sendFile(path.join(__dirname, "./views/comprar.html"))
 });
 
-app.get("/views/carrito-de-compras", (req, res)=>{
-<<<<<<< HEAD
+app.get("/views/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/formulario-registro.html"))
+ });
+
+ app.get("/views/carrito-de-compras.html", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/carrito-de-compras.html"))
-});
-=======
-    res.sendFile(path.join(__dirname, "./views/carrito-de-compras.html"))
-}) ;
->>>>>>> a6c95b45406dd3480326298ec7537a391b85e17a
+ });
