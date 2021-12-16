17201ba70921141284584b09251a41c19e4c034c
aguian <aguian0104@gmail.com>
2021-12-16 00:47:56 -0300

    se hizo multer commpleto, no pude avanzar con las validaciones

--- controller/userController.js
+++ controller/userController.js
@@ -13,15 +13,18 @@ const userController = {
     },
 
     processRegistrer: (req, res) => {
-return res.send("estas en post")
-    //     const resultValidation = validationResult(req);
-    //     if (resultValidation.errors.length > 0) {
-    //         return res.render('./users/formularioRegistro',{
-                
-    //         } errors: resultVlidator.mapped(),
-    //             oldDara: req.body
-    //         });
-    // },
+        return res.send({
+            body: req.body,
+            file: req.file
+        });
+        //     const resultValidation = validationResult(req);
+        //     if (resultValidation.errors.length > 0) {
+        //         return res.render('./users/formularioRegistro',{
+
+        //         } errors: resultVlidator.mapped(),
+        //             oldDara: req.body
+        //         });
+        // },
     },
     selecBuyOrSell: (req, res) => {
         res.render('./users/selecBuyOrSell');
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const multer = require("multer");
+const path = require('path');
 
+//multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null,'./public/img/avatars');
+    },
+    filename: (req, file, cb) => {
+        
+        let fileName = 'img_user' + Date.now() + path.extname(file.originalname);
+        cb(null, fileName);
+    }
+})
+const uploadFile = multer({storage});
 const userController = require('../controller/userController');
 
+// const validations = [
+//     body("first_name").notEmpty(),
+//     body("email").notEmpty(),
+//     body("last_name").notEmpty(),
+//     body("first_name").notEmpty(),
+
+// ]
 //ruta login
 router.get('/login', userController.login);
 // ruta del login al  home
@@ -11,7 +31,7 @@ router.post('/', userController.home);
 //formulario del registro
 router.get('/formularioRegistro', userController.registrer);
 //procesar el registro
-router.post('/formularioRegistro', userController.processRegistrer);
+router.post('/formularioRegistro',uploadFile.single('avatar'),validations, userController.processRegistrer);
 
 
 router.get('/selecBuyOrSell', userController.selecBuyOrSell)
