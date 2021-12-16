e5baafaf85a7b5af26ee96ab90d400f4f3bd8fed
alejandrolederman <alejandro.lederman@live.com.ar>
2021-12-15 21:47:55 -0300

    inpas

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,7 @@
-const bcryptjs = require ('bcryptjs');
-const {validationResult} = require ('express-validator');
+const bcryptjs = require('bcryptjs');
+const {
+    validationResult
+} = require('express-validator');
 const userController = {
 
     login: (req, res) => {
@@ -8,6 +10,17 @@ const userController = {
 
     registrer: (req, res) => {
         res.render('./users/formularioRegistro');
+    },
+
+    processRegistrer: (req, res) => {
+
+        const resultValidation = validationResult(req);
+        if (resultValidation.errors.length > 0) {
+            return res.render('./users/formularioRegistro',{
+                
+            } errors: resultVlidator.mapped(),
+                oldDara: req.body
+            });
     },
 
     selecBuyOrSell: (req, res) => {
