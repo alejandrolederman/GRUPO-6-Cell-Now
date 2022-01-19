685437db0557bff0392d3ec5d2b50efbf22eaa02
alejandrolederman <alejandro.lederman@live.com.ar>
2022-01-19 14:57:51 -0300

    implementacion sequelize

---
+++ database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "Cell-Now",
+    "password": null,
+    "database": "database_Cell-Now",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "Cell-Now",
+    "password": null,
+    "database": "database_Cell-Now",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "Cell-Now",
+    "password": null,
+    "database": "database_Cell-Now",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
