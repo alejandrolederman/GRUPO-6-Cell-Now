aa9b154132ae29f96bc6511c2443ff31c1737430
alejandrolederman <alejandro.lederman@live.com.ar>
2022-02-02 12:56:32 -0300

    guarda y loguea usuarios con detalle

--- controller/userController.js
+++ controller/userController.js
@@ -12,42 +12,26 @@ const userController = {
 
     /////////////////////////////////////////////
     usersList: (req, res) => {
-        res.render('./users/usersList', {
-            users
-        });
+        db.users.findAll()
+        .then(function(usuarios){
+            res.render('./users/usersList', {usuarios:usuarios});
+        })
+        .catch(function(err){
+            console.log(err)
+        }) 
     },
     
 ///////////////////////////////////////////////////
     usersDetail: (req, res) => {
-        let elId = req.params.id;
-        let usuario = users.find(unUsuario => {
-            if (unUsuario.id == elId) {
-                return unUsuario;
-            }
-        });
-        res.render('./users/usersDetail', {
-            usuario,
-            users
+        db.users.findByPk(req.params.id)
+        .then(function(usuario){
+            res.render('./users/usersDetail', {usuario:usuario})  
         });
     },
 
 ///////////////////////////////////////////////////////////    
-
-    // registrer: (req, res) => {
-
-    //     res.cookie("testing", "hola mundo", {maxAge: 1000 * 30})
-
-    //     res.render('./users/formularioRegistro');
-    // },
-
-    crear: function (req, res){
-        db.user_category.findAll()
-        .then(function(categoria){
-            return res.render("./users/formularioRegistro" , {categoria:categoria})
-        })
-        .catch(function(err){
-            console.log(err)
-        })
+    crear: function (req, res){       
+        return res.render("./users/formularioRegistro" )
     },
 
 ////////////////////////////////////////////////////////////////////////////
@@ -56,57 +40,38 @@ const userController = {
         if (resultValidation.errors.length > 0) {
 			return res.render('./users/formularioRegistro', {
 				errors: resultValidation.mapped(),
-				old: req.body 
+				old: req.body
 			});
 		}
 
-        let userInDB = db.users.findOne({where: { email: req.body.email }})
-        if (userInDB) {
-            		return res.render('./users/formularioRegistro', {
-            			errors: {
-            				email: {
-            					msg: 'Este email ya está registrado'
-            				}
-            			},
-            			old: req.body
-            		});
-            	}
-                else {
+        db.users.findOne({where: { email: req.body.email }})
+        .then( function(userInDB){
+            if (userInDB) {
+                return res.render('./users/formularioRegistro', {
+            	    errors: {
+            	    	email: {
+            	    		msg: 'Este email ya está registrado'
+            	    	}
+            	    },
+            	    old: req.body
+        	    });
+            }else {
                     
-                    user ={
-                first_name: req.body.first_name,
-                last_name: req.body.last_name,
-                user_name:req.body.user_name,
-                email: req.body.email,
-                password:bcryptjs.hashSync(req.body.password, 10),
-                category: req.body.user_category,
-                avatar: req.body.avatar
-                    }}
+                db.users.create({
+                    first_name: req.body.first_name,
+                    last_name: req.body.last_name,
+                    user_name:req.body.user_name,
+                    email: req.body.email,
+                    password:bcryptjs.hashSync(req.body.password, 10),
+                    category: req.body.user_category,
+                    avatar: req.body.avatar
+                })
+            }
 
-        return res.redirect('./login');
+            return res.redirect('./login');
+        });
     },
-    // processRegistrer: (req, res) => {
-		// const resultValidation = validationResult(req);
-	// 	let userInDB = User.findByField('email', req.body.email);
-
-	// 	if (userInDB) {
-	// 		return res.render('./users/formularioRegistro', {
-	// 			errors: {
-	// 				email: {
-	// 					msg: 'Este email ya está registrado'
-	// 				}
-	// 			},
-	// 			old: req.body
-	// 		});
-	// 	};
-
-	// 	let userToCreate = {
-	// 		...req.body,
-	// 		pass: bcryptjs.hashSync(req.body.pass, 10), // contraseña 1234 -> #$#FGGGRR$$#$% 
-	// 		avatar: req.file.filename
-	// 	};
-	// 	return res.redirect('./login');
-    // },
+    
 
 ///////////////////////////////////////////////////////////////////////////////////////////    
 
@@ -127,62 +92,55 @@ const userController = {
                 }
             });
         }
-
-        let userToLogin = db.users.findOne({where: { email: req.body.email }})
-
-        .then(function(usuario){
-
-        if(usuario){
-            let passOk = bcryptjs.compareSync(req.body.password, usuario.password)
-            if(passOk){
-                delete usuario.password;
-
-                req.session.userLogged = usuario;
-
-                if(usuario.category == "admin"){
-
-                    categoryAdmin = true;
+        //busca el mail en la base de datos
+        db.users.findOne({where: { email: req.body.email }})
+        .then(function(userToLogin){
+            
+            //si esta confirma que la contraseña sea correcta
+            if(userToLogin){
+                let passOk = bcryptjs.compareSync(req.body.password, userToLogin.password);        
+                if(passOk){ //si es correcta lo pone en seccion inicada 
+                    console.log(passOk);
+                    delete userToLogin.password;
+                    req.session.userLogged = userToLogin;
+                    //lo guarda en cookys
+                    if(req.body.remember) {
+                        res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 60 })
+                    };
+                    
+                    res.redirect('./userProfile');
                 }
 
-                if(req.body.remember) {
-
-					res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 60 })
-				}
-
-                return res.redirect('./userProfile');
+                res.render('./users/login', {
+                    errors: {
+                        pass: {
+                            msg: "las credenciales son invalidas"
+                        }
+                    }   
+                });
             }
 
-
-            return res.render('./users/login', {
-                errors: {
-                    pass: {
-                        msg: "las credenciales son invalidas"
-                    }
-                }   
-            });
-
-        }else{
-            return res.render('./users/login', {
+            res.render('./users/login', {
                 errors: {
                     email: {
                         msg: "no se encontro el email en nuestra base de datos"
                     }
                 }   
             });
-        }
-    })
-    },
+        })
 
+        .catch(function(err){
+            console.log(err)
+        })
+    },
 //////////////////////////////////////////////////////////////////////////////////////////////    
 
     profile: (req, res) => {
 
      return res.render('./users/userProfile', {
-			user: req.session.userLogged
+			users: req.session.userLogged
 		 });
 	},
-
-
 //////////////////////////////////////////////////////////////////////////////////////////////    
 
     logout: (req, res) => {
--- middlewares/validationRecord.js
+++ middlewares/validationRecord.js
@@ -8,8 +8,8 @@ const validationRecord = [
    body("first_name").notEmpty().withMessage("escribe tu nombre").bail(),
    body("last_name").notEmpty().withMessage("escribe tu apellido").bail(),
    body("email").notEmpty().withMessage("escribe tu mail").bail().isEmail().withMessage("debes escribir un formato de correo valido"),
-   body("username").notEmpty().withMessage("escribe tu nombre de usuario").bail(),
-   body("pass").notEmpty().withMessage("escribe una contraseña valida").bail(),
+   body("user_name").notEmpty().withMessage("escribe tu nombre de usuario").bail(),
+   body("password").notEmpty().withMessage("escribe una contraseña valida").bail(),
    body("avatar").custom((values, { req }) => {
 
       let file = req.file;
