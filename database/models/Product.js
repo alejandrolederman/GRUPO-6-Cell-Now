d3c73b884f0e16110f4a74fb82fe6e2724d9d4e4
alejandrolederman <alejandro.lederman@live.com.ar>
2022-02-14 09:44:25 -0300

    volvimos al commit anterior, falta ajustar js

--- database/models/Product.js
+++ database/models/Product.js
@@ -64,3 +64,74 @@ module.exports = (sequelize, dataTypes) => {
 
   return Product;
 };
+
+// module.exports = (sequelize, dataTypes) => {
+//   let alias = 'Dish';
+//   let cols = {
+//       id: {
+//           type: dataTypes.INTEGER,
+//           primaryKey: true,
+//           autoIncrement: true
+//       },
+//       name: dataTypes.STRING,
+//       description: dataTypes.STRING,
+//       price: dataTypes.DECIMAL,
+//       discount: dataTypes.INTEGER,
+//       recommended: dataTypes.INTEGER,
+//       image: dataTypes.STRING
+//   };
+//   /*let config = {
+//       tableName: 'Papachos',
+//       timestamps: false
+//   };*/
+      
+  
+//   const Dish = sequelize.define(alias, cols)
+//   //Aquí creo mi relación entre Platos (Diskes) y Categorias (Categories)
+//   Dish.associate = function(models) {
+//       Dish.belongsTo(models.Category, {
+//               as : 'category',
+//               foreignKey: 'categoryId'
+          
+//       });
+
+//       //Aquí hago la relación entre mi módelo Dish y mi tabla items  la cual contiene todo lo que el usuario está comprando
+//       Dish.hasMany(models.Item, {
+//           as: "items",
+//           foreignKey: "productId",
+//       });
+
+//       /*Dish.belongsTo(models.User, {
+//           as: "user",
+//           foreignKey: "userId",
+//       });*/
+
+//   };
+//   return Dish
+// }
+
+// module.exports = (sequelize, dataTypes) => {
+//   let alias = 'Category';
+//   let cols = {
+//       id: {
+//           type: dataTypes.INTEGER,
+//           primaryKey: true,
+//           autoIncrement: true
+//       },
+//       name: dataTypes.STRING,
+//   };
+//   /*let config = {
+//       tableName: 'categories',
+//       timestamps: false
+//   };*/
+//   const Category = sequelize.define(alias, cols) 
+//   //Aquí creo la relación con la tabla Dishes  - OJo: Relación de 1 a muchos
+//   Category.associate = function(models){
+//       Category.hasMany(models.Dish,{
+//               as: 'dishes',
+//               foreignKey: 'categoryId'})}   
+//   return Category
+// }
+
+
+
