dd55ab7a2945ef7347edd1cc733fcb0d0d3b6786
Daniela Kippes <danielakippes26@gmail.comm>
2022-02-25 12:46:50 -0300

    detail y swipper.js

---
+++ public/js/swiper.js
@@ -0,0 +1,12 @@
+
+    /* ==========================================================================
+         IMAGEN SLIDER + swiper
+         ========================================================================== */
+var swiper = new Swiper(".swiper-container", {
+    slidesPreView: 5.2,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable:true,
+    },
+});
