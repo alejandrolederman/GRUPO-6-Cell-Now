b3584b3e19901e291080177fc325e65da6681a0c
Daniela Kippes <danielakippes26@gmail.comm>
2022-02-26 19:48:58 -0300

    css y hatm home, detail

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -1,12 +1,17 @@
-
-    /* ==========================================================================
+const a = require("swiper")
+const swiper = this.swiper;
+swiper.loopDestroy();
+swiper.loopCreate();
+/* ==========================================================================
          IMAGEN SLIDER + swiper
          ========================================================================== */
 var swiper = new Swiper(".swiper-container", {
-    slidesPreView: 5.2,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable:true,
-    },
+  slidesPreView: 5.2,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
+
+
