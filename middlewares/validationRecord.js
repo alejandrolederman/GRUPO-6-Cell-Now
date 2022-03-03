a17094d8ad66c0586045e347682c9e31d4dbf1e2
aguian <aguian0104@gmail.com>
2022-03-03 20:47:05 -0300

    validacion back

--- middlewares/validationRecord.js
+++ middlewares/validationRecord.js
@@ -5,11 +5,11 @@ const { body } = require("express-validator");
 const path = require('path');
 
 const validationRecord = [
-   body("first_name").notEmpty().withMessage("escribe tu nombre").bail().isLength({min: 2}).withMessage('El nombre debe tener al menos 2 caracteres'),
-   body("last_name").notEmpty().withMessage("escribe tu apellido").bail().isLength({min: 2}).withMessage('El apellido debe tener al menos 2 caracteres'),
-   body("user_name").notEmpty().withMessage("escribe tu nombre de usuario").bail(),
-   body("email").notEmpty().withMessage("escribe tu mail").bail().isEmail().withMessage("debes escribir un formato de email valido"),
-   body("password").notEmpty().withMessage("escribe una contraseña valida").bail().isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres'),
+   body("first_name").notEmpty().withMessage("escribe tu nombre back").bail().isLength({min: 2}).withMessage('El nombre debe tener al menos 2 caracteres'),
+   body("last_name").notEmpty().withMessage("escribe tu apellido back").bail().isLength({min: 2}).withMessage('El apellido debe tener al menos 2 caracteres'),
+   body("user_name").notEmpty().withMessage("escribe tu nombre de usuario back").bail(),
+   body("email").notEmpty().withMessage("escribe tu mail back").bail().isEmail().withMessage("debes escribir un formato de email valido"),
+   body("password").notEmpty().withMessage("escribe una contraseña valida back").bail().isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres'),
    // body("user_category").notEmpty().withMessage("seleciones una categoria").bail(),
    body("avatar").custom((values, { req }) => {
 
--- middlewares/validatorLogin.js
+++ middlewares/validatorLogin.js
@@ -3,7 +3,7 @@ const { body } = require ("express-validator");
 
  const validationLogin = [
    
-  body("email").notEmpty().withMessage("escribe tu mail").bail().isEmail().withMessage("debes escribir un formato de email valido"),
+  body("email").notEmpty().withMessage("escribe tu mail back").bail().isEmail().withMessage("debes escribir un formato de email valido"),
    
   body("password").notEmpty().withMessage("escribe una contraseña valida").bail().isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres'),
     
